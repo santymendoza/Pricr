42ceb16ec804578b91434b23be2d3f36b6d72e79
Santy Mendoza <55068232+santymendoza@users.noreply.github.com>
2021-07-12 09:06:21 -0700

    Initial Commit

---
+++ Pricr/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Pricr
+//
+//  Created by Santy Mendoza on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Pricr/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Pricr
+//
+//  Created by Santy Mendoza on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
